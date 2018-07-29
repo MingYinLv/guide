f2e6d698665d33c77b98d6500f2b1fb8079e3048
lvmingyin <lvmingyin@vip.qq.com>
2018-07-29 20:44:59 +0800

    searchInput

--- Javascript/leetcode/searchInsert.js
+++ Javascript/leetcode/searchInsert.js
@@ -8,27 +8,13 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-
-  if (nums[0] > target) return 0;
-  if (nums[right] < target) return right + 1;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (target === nums[mid]) {
-      return mid;
-    } else if (target < nums[mid] && target > nums[mid - 1]) {
-      return mid;
-    } else if (target > nums[mid] && target < nums[mid + 1]) {
-      return mid + 1;
-    } else if (target < nums[mid]) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
+  const len = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] >= target) {
+      return i;
     }
   }
-  return -1;
+  return len;
 };
 
 
