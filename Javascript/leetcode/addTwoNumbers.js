78d350d219df59ae925565a3232eba1c7f7c99fb
lvmingyin <lvmingyin@vip.qq.com>
2018-09-10 09:09:15 +0800

    addTwoNumbers

--- Javascript/leetcode/addTwoNumbers.js
+++ Javascript/leetcode/addTwoNumbers.js
@@ -14,43 +14,45 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-  let l = 0;
-  let r = 0;
-  let index = 0;
-  while (l1 || l2) {
-    if (l1) {
-      l += l1.val * Math.pow(10, index);
-      l1 = l1.next;
-    }
-    if (l2) {
-      r += l2.val * Math.pow(10, index);
-      l2 = l2.next;
-    }
-    index++;
+  if (!l1 && !l2) return null;
+  else if (!l1) return l2;
+  else if (!l2) return l1;
+
+  let l1next = l1.next;
+  let l2next = l2.next;
+  let temp = l1.val + l2.val;
+  let add = Math.floor(temp / 10);
+  const result = new ListNode(temp % 10);
+  result.next = new ListNode();
+  let prev = result;
+  let next = result.next;
+  while (l1next || l2next) {
+    const l1val = l1next ? l1next.val : 0;
+    const l2val = l2next ? l2next.val : 0;
+    temp = l1val + l2val + add;
+    add = Math.floor(temp / 10);
+    next.val = temp % 10;
+    next.next = new ListNode();
+    prev = next;
+    next = next.next;
+    l1next = l1next ? l1next.next : null;
+    l2next = l2next ? l2next.next : null;
   }
-  let val = l + r;
-  const result = new ListNode(val % 10);
-  console.log(val);
-  let tmp = result;
-  val = Math.floor(val / 10);
-  while (val > 0) {
-    tmp.next = new ListNode(val % 10);
-    val = Math.floor(val / 10);
-    tmp = tmp.next;
+  if (add === 1) {
+    next.val = 1;
+  } else {
+    prev.next = null;
   }
   return result;
 };
 
-var l1 = new ListNode(1);
-l1.next = new ListNode(0);
-l1.next.next = new ListNode(0);
-l1.next.next.next = new ListNode(0);
-l1.next.next.next.next = new ListNode(0);
-l1.next.next.next.next.next = new ListNode(1);
+var l1 = new ListNode(9);
+l1.next = new ListNode(9);
+l1.next.next = new ListNode(9);
 
-var l2 = new ListNode(5);
-l2.next = new ListNode(6);
-l2.next.next = new ListNode(4);
+var l2 = new ListNode(9);
+l2.next = new ListNode(9);
+l2.next.next = new ListNode(9);
 //
 
 console.log(JSON.stringify(addTwoNumbers(l1, l2)));
