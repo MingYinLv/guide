493f75623b2e4aada6bab68a333a270e1bed0338
lvmingyin <lvmingyin@vip.qq.com>
2018-09-20 16:34:15 +0800

    isToeplitzMatrix

--- Javascript/leetcode/isToeplitzMatrix.js
+++ Javascript/leetcode/isToeplitzMatrix.js
@@ -11,29 +11,33 @@ var isToeplitzMatrix = function (matrix) {
   const r = matrix.length;
   const c = matrix[0].length;
 
-
-  function next(pois) {
-    if (!pois.length) return true;
-    let arr = [];
-    let val = matrix[pois[0][0]][pois[0][1]];
-    let result = true;
-    for (let i = 0; i < pois.length; i++) {
-      const [tmpR, tmpC] = pois[i];
-      if (val !== matrix[tmpR][tmpC]) {
-        result = false;
-      }
-
-      if (tmpR - 1 >= 0) {
-        arr.push([tmpR - 1, tmpC]);
-      }
-      if (tmpC + 1 < c) {
-        arr.push([tmpR, tmpC + 1]);
-      }
+  function isColumn(pois) {
+    const [tmpR, tmpC] = pois;
+    const rLen = matrix.length - tmpR;
+    const cLen = matrix[0].length - tmpC;
+    const min = rLen > cLen ? cLen : rLen;
+    const val = matrix[tmpR][tmpC];
+    for (let i = 1; i < min; i++) {
+      if (val !== matrix[tmpR + i][tmpC + i]) return false;
     }
-    return result && next(arr);
+    return true;
   }
 
-  return next([[r - 1, 0]]);
+  const max = r + c - 1;
+  for (let i = 0; i < max; i++) {
+    let x = i;
+    let y = 0;
+
+    if (i >= r) {
+      x = 0;
+      y = i - r + 1;
+    }
+    if (!isColumn([x, y])) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 console.log(isToeplitzMatrix([[19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26, 44, 41, 9, 18, 64], [55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26, 44, 41, 9, 18], [18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26, 44, 41, 9], [10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26, 44, 41], [44, 10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26, 44], [12, 44, 10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91, 26], [20, 12, 44, 10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39, 91], [20, 20, 12, 44, 10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56, 39], [48, 20, 20, 12, 44, 10, 18, 55, 19, 80, 23, 69, 95, 63, 94, 61, 78, 56]]));
